8c58d9974255616275bbc7ae510236366fa05399
almstrand <cristian.almstrand@gmail.com>
2014-12-18 22:19:40 -0800

    Updates test code to reflect new optional `cssMaxWidthProperty` parameter.

--- test/index.dart
+++ test/index.dart
@@ -2,13 +2,13 @@ import 'package:textent/textent.dart';
 
 main() {
 
-  Textent.measureTexts(["Measure me", "Me too!"], "150px arial,sans-serif").then((Map<String, TextSize> textSizes) {
-    Textent.measureTexts(["Measure me", "Me too!"], "150px arial,sans-serif").then((Map<String, TextSize> textSizes) {
+  Textent.measureTexts(["Measure me", "Me too!"], "150px arial,sans-serif", cssMaxWidthProperty: "14px").then((Map<String, TextSize> textSizes) {
+    Textent.measureTexts(["Measure me", "Me too!"], "150px arial,sans-serif", cssMaxWidthProperty: "14px").then((Map<String, TextSize> textSizes) {
       print("Our sizes are $textSizes");
     });
   });
 
-//  Textent.measureText("Measure me", "15px arial,sans-serif").then((TextSize textSize) {
-//    print("My size is $textSize");
-//  });
+  Textent.measureText("Measure me", "15px arial,sans-serif", cssMaxWidthProperty: "14px").then((TextSize textSize) {
+    print("My size is $textSize");
+  });
 }
